bcb638c0c76d503e32ccce86a95eb2c0f90a92d7
Stephen Gambill <stephensgambill@gmail.com>
2023-01-10 15:52:19 -0600

    My second version with updated HTML and CSS

---
+++ scripts/database.js
@@ -0,0 +1,20 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+const database = {
+    fish: [
+        {
+            name: "Bart",
+            species: "goldfish", 
+            food: "crustaceans",
+            size: 3,
+            harvested: "Brazil",
+
+        }
+    ]
+}
+
+console.log(database)
